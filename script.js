68783c4d0907e198125e11f0a7f2eb967474f737
ercumenlacin <1140104023@nku.edu.tr>
2020-11-03 12:00:48 +0300

    deneme

---
+++ script.js
@@ -0,0 +1,6 @@
+let menuToggle = document.querySelector('.menu-toggle');
+let navToggle = document.querySelector('.nav');
+menuToggle.addEventListener("click", function(){
+   menuToggle.classList.toggle("active");
+   navToggle.classList.toggle("active");
+                            })
